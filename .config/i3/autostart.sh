d7f67b23341a7991144191a2494bcdc6725cd39a
Eduard Romanyuk <eduard.romanyuk1990@gmail.com>
2017-02-18 12:32:26 +0200

    Update autostart.sh

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -2,5 +2,3 @@
 setxkbmap -layout us,ru -variant -option grp:alt_shift_toggle,grp_led:scroll
 feh --bg-scale ~/.config/i3/wall.png
 compton --config ~/.config/compton/compton.conf -b
-# polybar -c ~/.config/polybar/config top &
-# polybar -c ~/.config/polybar/config bottom &
